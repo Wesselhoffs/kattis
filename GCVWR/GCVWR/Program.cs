84c37c2fd943f4b9d99734436a1560ceb40ebddf
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-03 01:15:21 +0200

    Added GCVWR

---
+++ GCVWR/GCVWR/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GCVWR
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            int grossWeight = int.Parse(input[0]);
+            int truckWeight = int.Parse(input[1]);
+            int itemsToBring = int.Parse(input[2]);
+            int totalWeightOfItems = 0;
+
+            string[] weightOfItems = Console.ReadLine().Split(" ");
+
+            for (int i = 0; i < itemsToBring; i++)
+            {
+                totalWeightOfItems += int.Parse(weightOfItems[i]);
+            }
+
+            Console.WriteLine(((grossWeight - truckWeight) * 0.9) - totalWeightOfItems);
+        }
+    }
+}
