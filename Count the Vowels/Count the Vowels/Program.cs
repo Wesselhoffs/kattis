a8fd207a3f6834360eece4e02edc5c4ffd742310
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-02 11:10:26 +0200

    Count the Vowels added

---
+++ Count the Vowels/Count the Vowels/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Count_the_Vowels
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            char[] charInput = input.ToCharArray();
+            int vowels = 0;
+
+            foreach (char letter in charInput)
+            {
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    vowels++;
+                }
+            }
+            Console.WriteLine(vowels);
+        }
+    }
+}
