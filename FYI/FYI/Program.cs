dee70a2c63ba66910e60ec720ec7e1da9dc77d9b
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-03 01:50:53 +0200

    Added FYI

---
+++ FYI/FYI/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FYI
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string telephoneNr = Console.ReadLine();
+            char[] arrayOfnumber = telephoneNr.ToCharArray();
+
+            if (arrayOfnumber[0] == '5' && arrayOfnumber[1] == '5' && arrayOfnumber[2] == '5')
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
