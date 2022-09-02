8524e4d52d84a8d501432715bd6630903d74ce2d
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-02 10:43:44 +0200

    Added Apaxians solution

---
+++ Apaxiaaaaaaaaaaaans!/Apaxiaaaaaaaaaaaans!/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Apaxiaaaaaaaaaaaans_
+{
+    using System;
+    using System.Text;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputName = Console.ReadLine();            
+            char[] charsInName = inputName.ToCharArray();
+
+            StringBuilder outputName = new StringBuilder();            
+            char tempChar = charsInName[0];
+            outputName.Append(tempChar);          
+            int nameLenght = charsInName.Length;          
+
+            for (int i = 0; i < nameLenght; i++)
+            {
+                if (tempChar != charsInName[i])
+                {
+                    outputName.Append(charsInName[i]);
+                    tempChar = charsInName[i];
+                }
+            }
+
+            Console.WriteLine(outputName.ToString());
+            
+        }
+    }
+}
