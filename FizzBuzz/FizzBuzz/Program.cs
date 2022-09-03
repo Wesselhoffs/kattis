d00b2f556c00aac335173dc93535e7a43b739bfd
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-03 02:06:59 +0200

    Added FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+            int n = int.Parse(input[2]);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % x == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % y == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
