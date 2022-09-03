15c1e9ada2cde4a012e609877cfd319b72e03561
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-03 15:30:31 +0200

    Added Finding An A

---
+++ Finding An A/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Finding_An_A
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();;
+            Console.WriteLine(input.Substring(input.IndexOf("a")));
+        }
+    }
+}
