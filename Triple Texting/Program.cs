1c375968343904e5fa43ee101c7eb2faea51de59
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-06 20:04:07 +0200

    Added Tripple Texting

---
+++ Triple Texting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Triple_Texting
+{
+    using System;
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int wordLenght = input.Length / 3;
+
+            char[] firstWord = input.Substring(0, wordLenght).ToCharArray();
+            char[] secondWord = input.Substring(wordLenght, wordLenght).ToCharArray();
+            char[] thirdWord = input.Substring(wordLenght * 2, wordLenght).ToCharArray();
+            char[] correctWord = new char[wordLenght];
+
+            for (int i = 0; i < wordLenght; i++)
+            {
+                if (firstWord[i] == secondWord[i] && firstWord[i] == thirdWord[i]) { correctWord[i] = firstWord[i]; }
+                else if (firstWord[i] == secondWord[i]) { correctWord[i] = secondWord[i]; }
+                else if (firstWord[i] == thirdWord[i]) { correctWord[i] = firstWord[i]; }
+                else if (secondWord[i] == thirdWord[i]) { correctWord[i] = secondWord[i]; }
+            }
+            string result = new string(correctWord);
+            Console.WriteLine(result);
+
+        }
+    }
+}
