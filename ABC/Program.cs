82595afc3c7cf9828669522447f98a91ef04fccb
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-07 20:19:53 +0200

    Added ABC

---
+++ ABC/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ABC
+{
+    using System;
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            char[] chars = Console.ReadLine().ToCharArray();
+            int[] sortedArray = new int[numbers.Length];
+
+            int alocation = Array.IndexOf(chars, 'A');
+            int bLocation = Array.IndexOf(chars, 'B');
+            int cLocation = Array.IndexOf(chars, 'C');
+            Array.Sort(numbers);
+            sortedArray[alocation] = numbers[0];
+            sortedArray[bLocation] = numbers[1];
+            sortedArray[cLocation] = numbers[2];
+
+            Console.WriteLine($"{sortedArray[0]} {sortedArray[1]} {sortedArray[2]}");
+        }
+    }
+}
