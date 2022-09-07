9b66290fb068135dcd343eb2bae1177aaed30f0e
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-07 22:13:08 +0200

    Added Backspace, working in VS, but getting memory limit exceeded due to kattis limits.

---
+++ Backspace/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Backspace
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            LessThanBackspace(input);
+        }
+
+        static void LessThanBackspace(string input)
+        {
+            int locationOfLessthan = input.IndexOf('<');
+            if (locationOfLessthan != -1)
+            {
+                string firstPartOfString = input.Substring(0, locationOfLessthan - 1);
+                string secondPartOfString = input.Substring(locationOfLessthan + 1);
+                string newString = firstPartOfString + secondPartOfString;
+                LessThanBackspace(newString);
+
+            }
+            else
+            {
+                Console.WriteLine(input);
+            }
+        }
+
+    }
+}
+
+// This solution is poorly optimized. I get "Memory Limit Exceeded" in kattis test sequence.
