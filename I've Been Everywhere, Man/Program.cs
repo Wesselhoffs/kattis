c1557b5f98bfee09baf257137076efa6cb893100
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-10 15:54:45 +0200

    Added I've Been Everywhere, Man

---
+++ I've Been Everywhere, Man/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Everywhere
+{
+    using System;
+    using System.Collections.Generic;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTestCases, workTripsTaken;
+            List<int> citiesVisited = new List<int>();
+            numberOfTestCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfTestCases; i++)
+            {
+                workTripsTaken = int.Parse(Console.ReadLine());
+                Dictionary<string, int> cities = new Dictionary<string, int>();
+                int counter = 0;
+                for (int x = 0; x < workTripsTaken; x++)
+                {
+                    string tempString = Console.ReadLine();
+
+                    if (cities.ContainsKey(tempString))
+                    {
+                        // Do nothing!!
+                    }
+                    else
+                    {
+                        cities.Add(tempString, 1);
+                        counter++;
+                    }
+                }
+                citiesVisited.Add(counter);
+            }
+            PrintUniqueCities(citiesVisited);
+        }
+        static void PrintUniqueCities(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
